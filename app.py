c168a8101c06ee5d2188e75144d8739d482b2ccd
adityalakhani <lakhaniaditya3@gmail.com>
2025-06-20 23:13:44 +0530

    updated app.py

--- app.py
+++ app.py
@@ -105,5 +105,10 @@ def display_page(pathname):
 
     return navbar, home.layout
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=False)
